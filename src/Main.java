516927c75750e37a8d8fa8198e1eae460185640c
Feng Bo <bobbyfeng@cse.com.sg>
2017-12-09 22:14:10 +0800

    lambda expressions 2

--- src/Main.java
+++ src/Main.java
@@ -18,33 +18,53 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		new Thread(()-> {
-			System.out.println("Printing from the Runnable");
-			System.out.println("Line 2");
-			System.out.format("This is line %d\n", 3);
-		}).start();
-		
+		// new Thread(() -> {
+		// System.out.println("Printing from the Runnable");
+		// System.out.println("Line 2");
+		// System.out.format("This is line %d\n", 3);
+		// }).start();
+
 		Employee john = new Employee("John Doe", 30);
 		Employee tim = new Employee("Tim Buchalka", 21);
 		Employee jack = new Employee("Jack Hill", 40);
 		Employee snow = new Employee("Snow White", 22);
-		
+
 		List<Employee> employees = new ArrayList<>();
 		employees.add(john);
 		employees.add(tim);
 		employees.add(jack);
 		employees.add(snow);
-		
-		Collections.sort(employees, new Comparator<Employee>() {
-			@Override
-			public int compare(Employee o1, Employee o2) {
-				return o1.getName().compareTo(o2.getName());
-			}
-		});
-		
-		for(Employee employee : employees) {
+
+		// Collections.sort(employees, new Comparator<Employee>() {
+		// @Override
+		// public int compare(Employee o1, Employee o2) {
+		// return o1.getName().compareTo(o2.getName());
+		// }
+		// });
+
+		Collections.sort(employees,
+				(Employee employee1, Employee employee2) -> employee1.getName().compareTo(employee2.getName()));
+
+		for (Employee employee : employees) {
 			System.out.println(employee.getName());
 		}
+
+		// String sillyString = doStringStuff(new UpperConcat() {
+		//
+		// @Override
+		// public String upperAndConcat(String s1, String s2) {
+		// return s1.toUpperCase() + s2.toUpperCase();
+		// }
+		// }, employees.get(0).getName(), employees.get(1).getName());
+		// System.out.println(sillyString);
+
+		String sillyString = doStringStuff((s1, s2) -> s1.toUpperCase() + s2.toUpperCase(),
+				employees.get(0).getName(), employees.get(1).getName());
+		System.out.println(sillyString);
+	}
+
+	public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+		return uc.upperAndConcat(s1, s2);
 	}
 
 }
@@ -52,7 +72,7 @@ public class Main {
 class Employee {
 	private String name;
 	private int age;
-	
+
 	public Employee(String name, int age) {
 		this.name = name;
 		this.age = age;
@@ -66,7 +86,8 @@ class Employee {
 	}
 
 	/**
-	 * @param name the name to set
+	 * @param name
+	 *            the name to set
 	 */
 	public void setName(String name) {
 		this.name = name;
@@ -80,13 +101,17 @@ class Employee {
 	}
 
 	/**
-	 * @param age the age to set
+	 * @param age
+	 *            the age to set
 	 */
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
-	
+
+}
+
+interface UpperConcat {
+	public String upperAndConcat(String s1, String s2);
 }
 
 class CodeToRun implements Runnable {
@@ -94,7 +119,7 @@ class CodeToRun implements Runnable {
 	@Override
 	public void run() {
 		System.out.println("Printing from the Runnable");
-		
+
 	}
-	
+
 }
