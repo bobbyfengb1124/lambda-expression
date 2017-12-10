177d679294c3b1d82892e93a90562ce20b17a9b1
Feng Bo <bobbyfeng@cse.com.sg>
2017-12-10 22:12:25 +0800

    lambda expressions nested blocks

--- src/Main.java
+++ src/Main.java
@@ -18,49 +18,58 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		// new Thread(() -> {
-		// System.out.println("Printing from the Runnable");
-		// System.out.println("Line 2");
-		// System.out.format("This is line %d\n", 3);
-		// }).start();
-
-		Employee john = new Employee("John Doe", 30);
-		Employee tim = new Employee("Tim Buchalka", 21);
-		Employee jack = new Employee("Jack Hill", 40);
-		Employee snow = new Employee("Snow White", 22);
-
-		List<Employee> employees = new ArrayList<>();
-		employees.add(john);
-		employees.add(tim);
-		employees.add(jack);
-		employees.add(snow);
-
-		// Collections.sort(employees, new Comparator<Employee>() {
-		// @Override
-		// public int compare(Employee o1, Employee o2) {
-		// return o1.getName().compareTo(o2.getName());
-		// }
-		// });
-
-		Collections.sort(employees,
-				(Employee employee1, Employee employee2) -> employee1.getName().compareTo(employee2.getName()));
-
-		for (Employee employee : employees) {
-			System.out.println(employee.getName());
-		}
-
-		// String sillyString = doStringStuff(new UpperConcat() {
+		// // new Thread(() -> {
+		// // System.out.println("Printing from the Runnable");
+		// // System.out.println("Line 2");
+		// // System.out.format("This is line %d\n", 3);
+		// // }).start();
 		//
-		// @Override
-		// public String upperAndConcat(String s1, String s2) {
-		// return s1.toUpperCase() + s2.toUpperCase();
+		// Employee john = new Employee("John Doe", 30);
+		// Employee tim = new Employee("Tim Buchalka", 21);
+		// Employee jack = new Employee("Jack Hill", 40);
+		// Employee snow = new Employee("Snow White", 22);
+		//
+		// List<Employee> employees = new ArrayList<>();
+		// employees.add(john);
+		// employees.add(tim);
+		// employees.add(jack);
+		// employees.add(snow);
+		//
+		// // Collections.sort(employees, new Comparator<Employee>() {
+		// // @Override
+		// // public int compare(Employee o1, Employee o2) {
+		// // return o1.getName().compareTo(o2.getName());
+		// // }
+		// // });
+		//
+		// Collections.sort(employees,
+		// (Employee employee1, Employee employee2) ->
+		// employee1.getName().compareTo(employee2.getName()));
+		//
+		// for (Employee employee : employees) {
+		// System.out.println(employee.getName());
 		// }
-		// }, employees.get(0).getName(), employees.get(1).getName());
+		//
+		// // String sillyString = doStringStuff(new UpperConcat() {
+		// //
+		// // @Override
+		// // public String upperAndConcat(String s1, String s2) {
+		// // return s1.toUpperCase() + s2.toUpperCase();
+		// // }
+		// // }, employees.get(0).getName(), employees.get(1).getName());
+		// // System.out.println(sillyString);
+		//
+		// UpperConcat uc = (s1, s2) -> {
+		// String result = s1.toUpperCase() + s2.toUpperCase();
+		// return result;
+		// };
+		// String sillyString = doStringStuff(uc,
+		// employees.get(0).getName(), employees.get(1).getName());
 		// System.out.println(sillyString);
 
-		String sillyString = doStringStuff((s1, s2) -> s1.toUpperCase() + s2.toUpperCase(),
-				employees.get(0).getName(), employees.get(1).getName());
-		System.out.println(sillyString);
+		AnotherClass anotherClass = new AnotherClass();
+		String s = anotherClass.doSomething();
+		System.out.println(s);
 	}
 
 	public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
@@ -122,4 +131,21 @@ class CodeToRun implements Runnable {
 
 	}
 
+}
+
+class AnotherClass {
+
+	public String doSomething() {
+		int i = 0;
+		UpperConcat uc = (s1, s2) -> {
+			System.out.println("The Lambda expression's class is: " + getClass().getSimpleName());
+			String result = s1.toUpperCase() + s2.toUpperCase();
+			return result;
+		};
+		System.out.println("The AnotherClass class's name is: " + getClass().getSimpleName());
+
+		return Main.doStringStuff(uc, "String1", "String2");
+
+
+	}
 }
